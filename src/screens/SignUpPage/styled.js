4ff16889b5ba1dc24411a41068ee41003c2d6349
Giovanna Caivano <giovannadottocaivano@gmail.com>
2020-10-25 07:27:16 -0300

    organizing styled comp

---
+++ src/screens/SignUpPage/styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const ErrorText = styled.p`
+font-family: Roboto;
+font-size: 12px;
+font-weight: normal;
+font-stretch: normal;
+font-style: normal;
+line-height: normal;
+letter-spacing: -0.29px;
+color: #e02020;
+text-align: left;
+`
